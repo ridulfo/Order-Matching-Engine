f29d749bad0b659a4a0ddbfe3d341a8f2456d101
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2020-03-16 20:41:21 +0100

    Adding benchmark.py

---
+++ Benchmark.py
@@ -0,0 +1,31 @@
+from Orderbook import Orderbook
+from Order import *
+from Trade import Trade
+from random import getrandbits, randint, random
+
+# Benchmark
+OB = Orderbook()
+numOrders = 10**7
+orders = []
+for n in range(numOrders):
+	if bool(getrandbits(1)):
+		orders.append(LimitOrder(n, Side.BUY, randint(1, 200), randint(1, 4)))
+	else:
+		orders.append(LimitOrder(n, Side.SELL, randint(1, 200), randint(1, 4)))
+
+from time import time
+start = time()
+for order in orders:
+	OB.processOrder(order)
+end = time()
+totalTime = (end-start)
+print("Time: " + str(totalTime))
+print("Time per order (us): " + str(1000000*totalTime/numOrders))
+print("Orders per second: " + str(numOrders/totalTime))
+
+"""
+Output
+Time: 25.271270990371704
+Time per order (us): 2.5271270990371706
+Orders per second: 395706.25489354995
+"""
